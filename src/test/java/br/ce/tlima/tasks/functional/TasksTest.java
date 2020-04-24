cda65534ef77b9efc6299fd29182ad1c72d7f873
Thiago Lima <thiaogat.lima1985@gmail.com>
2020-04-24 11:55:27 -0300

    Healt Check

--- src/test/java/br/ce/tlima/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/tlima/tasks/functional/TasksTest.java
@@ -1,22 +1,23 @@
 package br.ce.tlima.tasks.functional;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+//import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-//		WebDriver driver = new ChromeDriver();
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), cap);
+		WebDriver driver = new ChromeDriver();
+//		DesiredCapabilities cap = DesiredCapabilities.chrome();
+//		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), cap);
 		driver.navigate().to("http://192.168.0.104:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
